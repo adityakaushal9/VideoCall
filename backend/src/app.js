874fa71e1ce51297bc89948f6a7d905620fe1683
adityakaushal9 <adityakaushal716@gmail.com>
2025-12-28 15:52:49 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,16 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://videocall-1-5nhn.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
